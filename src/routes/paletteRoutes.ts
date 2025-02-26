b93745cb7cdb8debe596f279c43f1cee13abe4d2
Ramiro <alu0101425030@ull.edu.es>
2025-02-26 14:27:28 +0000

    TODO add palette routes

---
+++ src/routes/paletteRoutes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+// import { createPalette, getPublicPalettes } from '../controllers/paletteController';
+// import { authMiddleware } from '../middlewares/authMiddleware';
+
+const router = express.Router();
+
+// router.post('/', authMiddleware, createPalette);
+// router.get('/public', getPublicPalettes);
+
+export default router;
