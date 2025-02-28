d67fdeaf419146d48f39dd86bb311529f0f87a61
Ramiro <alu0101425030@ull.edu.es>
2025-02-28 12:22:36 +0000

    paletteRouted added

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import authRoutes from './authRoutes';
+import paletteRoutes from './paletteRoutes';
 
 const router = express.Router();
 
 export default (): express.Router => {
     authRoutes(router);
+    paletteRoutes(router);
     return router;
 };
