d100e77bab9306cb8af27f8a6d4c5e7f40cff9db
Ramiro <alu0101425030@ull.edu.es>
2025-02-28 12:22:25 +0000

    "/" route added

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import { login, register, protectedSession, logout } from '../controllers/authControllers'
+import { login, register, protectedSession, logout, defaultController } from '../controllers/authControllers'
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 export default (router: express.Router) => {
     router.post("/register", register);
-    router.post("/login", login);
-    router.get("/protected", protectedSession);
     router.post("/logout", logout);
+    router.post("/login", login);
+    router.use(authMiddleware);
+    router.get("/protected", protectedSession);
+    router.get("/", defaultController);
 };
 
 
