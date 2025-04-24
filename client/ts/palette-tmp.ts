94fec64d1541e863d7c16c8d6237a01803ea9a94
Ramiro <alu0101425030@ull.edu.es>
2025-04-24 02:41:21 +0100

    palette-tmp second version

--- client/ts/palette-tmp.ts
+++ client/ts/palette-tmp.ts
@@ -1,8 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Contenedores de colores
     const colorContainers = document.querySelectorAll<HTMLElement>(".color-container");
-    // Sliders de color globales
-    const colorSection = document.querySelector<HTMLSelectElement>(".color-section");
+    const colorScheme = document.querySelector<HTMLSelectElement>(".color-type");
+
+    const satSlider1 = document.querySelector<HTMLInputElement>("#s-1");
+    const lightSlider1 = document.querySelector<HTMLInputElement>("#l-1");
+    const hueSlider1 = document.querySelector<HTMLInputElement>("#h");
+    const satSlider2 = document.querySelector<HTMLInputElement>("#s-2");
+    const lightSlider2 = document.querySelector<HTMLInputElement>("#l-2");
+    const satValue1 = document.querySelector<HTMLInputElement>("#s-1-value");
+    const lightValue1 = document.querySelector<HTMLInputElement>("#l-1-value");
+    const hueValue1 = document.querySelector<HTMLInputElement>("#h-value");
+    const satValue2 = document.querySelector<HTMLInputElement>("#s-2-value");
+    const lightValue2 = document.querySelector<HTMLInputElement>("#l-2-value");
+    const analogousSlider = document.querySelector<HTMLInputElement>("#analogous");
+    const splitSlider = document.querySelector<HTMLInputElement>("#split");
+    const triadSlider = document.querySelector<HTMLInputElement>("#triad");
+    const complementarySlider = document.querySelector<HTMLInputElement>("#complementary");
+    const analogousValue = document.querySelector<HTMLInputElement>("#analogous-value");
+    const splitValue = document.querySelector<HTMLInputElement>("#split-value");
+    const triadValue = document.querySelector<HTMLInputElement>("#triad-value");
+    const complementaryValue = document.querySelector<HTMLInputElement>("#complementary-value");
+
     // Función para sincronizar slider y número
     const connectSliderWithNumber = (
       slider: HTMLInputElement,
@@ -24,26 +43,23 @@ document.addEventListener("DOMContentLoaded", () => {
         onChange();
       });
     };
-    const satSlider1 = document.querySelector<HTMLInputElement>("#s-1");
-    const lightSlider1 = document.querySelector<HTMLInputElement>("#l-1");
-    const hueSlider = document.querySelector<HTMLInputElement>("#h");
-    const satSlider2 = document.querySelector<HTMLInputElement>("#s-2");
-    const lightSlider2 = document.querySelector<HTMLInputElement>("#l-2");
-    const satValue1 = document.querySelector<HTMLInputElement>("#s-1-value");
-    const lightValue1 = document.querySelector<HTMLInputElement>("#l-1-value");
-    const hueValue = document.querySelector<HTMLInputElement>("#h-value");
-    const satValue2 = document.querySelector<HTMLInputElement>("#s-2-value");
-    const lightValue2 = document.querySelector<HTMLInputElement>("#l-2-value");
-    if (!satSlider1 || !lightSlider1 || !hueSlider ||
+    if (!satSlider1 || !lightSlider1 || !hueSlider1 ||
         !satSlider2 || !lightSlider2 || !satValue1 || !lightValue1 ||
-        !hueValue || !satValue2 || !lightValue2) {
+        !hueValue1 || !satValue2 || !lightValue2 || !analogousSlider ||
+        !splitSlider || !triadSlider || !complementarySlider || !analogousValue ||
+        !splitValue || !triadValue || !complementaryValue) {
         return;
     }
     connectSliderWithNumber(satSlider1, satValue1, updateAll);
     connectSliderWithNumber(lightSlider1, lightValue1, updateAll);
-    connectSliderWithNumber(hueSlider, hueValue, updateAll);
+    connectSliderWithNumber(hueSlider1, hueValue1, updateAll);
     connectSliderWithNumber(satSlider2, satValue2, updateAll);
     connectSliderWithNumber(lightSlider2, lightValue2, updateAll);
+
+    connectSliderWithNumber(analogousSlider, analogousValue, updateAll);
+    connectSliderWithNumber(triadSlider, triadValue, updateAll);
+    connectSliderWithNumber(complementarySlider, complementaryValue, updateAll);
+    connectSliderWithNumber(splitSlider, splitValue, updateAll);
 
     colorContainers.forEach((container) => {
       const colorDiv = container.querySelector<HTMLElement>(".color");
@@ -51,7 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
       if (!colorDiv || !button) return;
   
-      const colorId = colorDiv.id.split("-")[1]; // "1", "2", etc.
+      let colorId = colorDiv.id.split("-")[1]; // "1", "2", etc.
   
       const panel = container.querySelector<HTMLElement>(`#panel-${colorId}`);
       const hueSlider = container.querySelector<HTMLInputElement>(`#hue-${colorId}`);
@@ -80,80 +96,67 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         updateAll();
       });
-      // Función para calcular los colores
-      function calculateColors(colorText: string) : string[] {
-        let colorNumber = parseInt(colorText); // Obtener el número del color
-        const hueInput = document.getElementById(`hue-${colorNumber}`) as HTMLInputElement;
-        const inputs : HTMLInputElement[] = [];        
-        for (let i = 0; i < 5; i++, colorNumber++) {
-          if (colorNumber > 5) {
-            colorNumber = 1;
-          }
-          inputs.push(document.getElementById(`hue-${colorNumber}`) as HTMLInputElement);
-          inputs.push(document.getElementById(`hue-value-${colorNumber}`) as HTMLInputElement);
-        }
-        const hue = parseInt(hueInput.value); // Obtener el valor del slider de tono
-        const colors : string[] = [];
-        const colorType = document.querySelector<HTMLSelectElement>(".color-type");
-        switch (colorType?.value) {
-            case "analogous":
-                // Colores análogos (Hue ±30°)
-                for (let i = 0; i < 5; i++) {
-                  const tmp = (hue + (i * 10)) % 360;
-                  colors.push(tmp.toString());
-                }
-                for (let i = 2; i < inputs.length; i+=2) {
-                  inputs.at(i)?.setAttribute("value", `${(hue + (i / 2 * 10)) % 360}`);
-                  inputs.at(i + 1)?.setAttribute("value", `${(hue + (i / 2 * 10)) % 360}`);
-                }
-                break;
-            case "complementary":
-                // Colores complementarios (Hue +180°)
-                // colors.push(hue);
-                // colors.push((hue + 180) % 360);
-                break;
-            case "split-complementary":
-                // Split complementarios (Hue ±150°)
-                // colors.push(hue);
-                // colors.push((hue + 30) % 360);
-                // colors.push((hue - 30 + 360) % 360);
-                break;
-            case "triad":
-                // Triadas (Hue ±120°)
-                // colors.push(hue);
-                // colors.push((hue + 120) % 360);
-                // colors.push((hue - 120 + 360) % 360);
-                break;
-            case "square":
-                // Cuadrado (Hue ±90° y ±180°)
-                // colors.push(hue);
-                // colors.push((hue + 90) % 360);
-                // colors.push((hue + 180) % 360);
-                // colors.push((hue - 90 + 360) % 360);
-                break;
-            case "monochromatic":
-                // Monocromático (solo el tono base)
-                // colors.push(hue);
-                break;
-            
-            default:
-                // Si no hay esquema, solo devuelve el tono base
-                // colors.push(hue);
-        }
-        return colors;
-      }
+
       // Función que actualiza el color en CSS
       const updateColor = () => {
+        console.log(colorId);
+        const parent = document.getElementById(`color-checkbox-${colorId}`)?.parentElement?.id;
+        
+        if (!parent) return;
         const s = satSlider.value;
         const l = lightSlider.value;
-        const hues = calculateColors(colorId);
-        let colorIdNumber = parseInt(colorId);
-        for (let i = 0; i < 5; i++, colorIdNumber++) {
-          if (colorIdNumber > 5) {
-            colorIdNumber = 1;
-          }
-          document.documentElement.style.setProperty(`--color-${colorIdNumber}`, `hsl(${hues[i]}, ${s}%, ${l}%)`);
+        const h = hueSlider.value;
+        if (parent === "palette-1") {
+          satSlider1.value = s;
+          satValue1.value = s;
+          lightSlider1.value = l;
+          lightValue1.value = l;
+        } else {
+          satSlider2.value = s;
+          satValue2.value = s;
+          lightSlider2.value = l;
+          lightValue2.value = l;
         }
+        let hueNumber: number = Number(hueSlider1.value) - Number(hueSlider.value);
+        switch (colorScheme!.value) {
+          case "analogous":
+            // Colores análogos (Hue ±30°)
+            const angleAnalogous = Number(analogousSlider.value);
+            hueNumber = (hueNumber + angleAnalogous) % 360;
+            break;
+          case "complementary":
+              // Colores complementarios (Hue +180°)
+              const angleComplementary = Number(complementarySlider.value);
+              hueNumber = (hueNumber + angleComplementary) % 360;
+              break;
+          case "split-complementary":
+              // Split complementarios (Hue ±150°)
+              const angleSplit = Number(splitSlider.value);
+              hueNumber = (hueNumber + angleSplit) % 360;
+              break;
+          case "triad":
+              // Triadas (Hue ±120°)
+              const angleTriad = Number(triadSlider.value);
+              hueNumber = (hueNumber + angleTriad) % 360;
+              break;
+          case "square":
+              // Cuadrado (Hue ±90° y ±180°)
+              hueNumber = (hueNumber + 90) % 360;
+              break;
+        }
+
+        // Calculos necesarios para actualizar el branding color
+        // ya que desde el branding color se calcula el resto de colores
+        if (colorId !== "1") {
+          hueNumber = (hueNumber * -1 + Number(hueSlider1.value));
+          // Normalizamos
+          hueNumber = (hueNumber % 360 + 360) % 360;
+          hueNumber = Math.max(0, Math.min(hueNumber, 360));
+        } else {
+          hueNumber = Number(h);
+        }
+        hueSlider1.value = hueNumber.toString();
+        hueValue1.value = hueNumber.toString();
         updateAll();
       };
   
@@ -163,7 +166,8 @@ document.addEventListener("DOMContentLoaded", () => {
       connectSliderWithNumber(lightSlider, lightNumber, updateColor);
   
       // Inicializa el color
-      updateColor();
+      updateAll();
     });
+
 });
   
